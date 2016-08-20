a79f95c2f5da2be7d26accb38c6bf81757b53641
Melissa414 <Missy41491@gmail.com>
2016-08-20 14:49:18 -0400

    initial commit

---
+++ assests/javascript/game.js
@@ -0,0 +1,17 @@
+			
+		var bobafett= {name="Boba Fet", health="110", attack="6", counter="8"};
+		var darthsidious= {name="Darth Sidious", health="130", attack="6", counter="10"};
+		var lukeskywalker= {name="Luke Skywaker", health="110", attack="6", counter="7"};
+		var darthvader= {name="Darth Vader", health="125", attack="6", counter="10"};
+
+
+$( document ).ready(function(){
+
+	$( '.players' ).on('click', function(){
+			// console.log("hello");
+		
+
+		
+
+	});
+});
