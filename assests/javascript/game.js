ae6259f272d4de3d1ef21e6c0099247efa72f4a5
Melissa414 <Missy41491@gmail.com>
2016-08-21 14:33:53 -0400

    second draft

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,17 +1,43 @@
-			
-		var bobafett= {name="Boba Fet", health="110", attack="6", counter="8"};
-		var darthsidious= {name="Darth Sidious", health="130", attack="6", counter="10"};
-		var lukeskywalker= {name="Luke Skywaker", health="110", attack="6", counter="7"};
-		var darthvader= {name="Darth Vader", health="125", attack="6", counter="10"};
 
+$(document).ready(function(){
 
-$( document ).ready(function(){
+var bobaFett= [name="Boba Fet", attack="6", counter="8"];
+var darthSidious= [name="Darth Sidious", attack="6", counter="10"];
+var lukeSkywalker= [name="Luke Skywaker", attack="6", counter="7"];		
+var darthVader= [name="Darth Vader", attack="6", counter="10"];
+var playerHealth=115;
+var computerHealth=110;
 
-	$( '.players' ).on('click', function(){
-			// console.log("hello");
+	// for (var i = players.length * 6; i >= 0; i--) {
+	// 	players[i]
+	// }
+
+	//boba fight
+	$("#bobaFett").click(function(){
+		
 		
 
-		
 
 	});
+	//emperor poopatine fight
+	$("#darthSidious").click(function(){
+	
+
+
+	});
+	//luke fight
+	$("#lukeSkywalker").click(function(){
+
+
+
+	});
+	//vader fight
+	$("#darthVader").click(function(){
+
+
+
+
+	});
+
+	
 });
