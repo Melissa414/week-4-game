9b192fd2f4db9e46338e017a1f9955b1e0da5cc3
Melissa414 <Missy41491@gmail.com>
2016-08-23 23:09:01 -0400

    fifth draft

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,76 +1,89 @@
 
 $(document).ready(function(){
 
-var bobaFett= [name='Boba Fet', attack='6', counter='8'];
-var darthSidious= [name='Darth Sidious', attack='6', counter='10'];
-var lukeSkywalker= [name='Luke Skywaker', attack='6', counter='7'];		
-var darthVader= [name='Darth Vader', attack='6', counter='10'];
-var playerHealth=115;
-var computerHealth=110;
+var bobaFett= {
+		name: 'Boba Fett', 
+		health: 95, 
+		attk: 6, 
+		counterattk: 7}
+
+var darthSidious= {
+		name:'Darth Sidious', 
+		health: 120, 
+		attk: 6, 
+		counterattk: 10}
+
+var lukeSkywalker= {
+		name:'Luke Skywaker', 
+		health: 115, 
+		attack: 6, 
+		counterattk: 8}	
+
+var darthVader= {
+		name:'Darth Vader',
+		health: 105, 
+		attk: 6, 
+		counterattk: 8}
 
 
 //music playing
 var audio = new Audio('assests/music/starwars.mp3');
 audio.play();
 
+// var yourCharacter = $('#yourCharacter');
 
-
-	//picking boba fight
-	$('.bobafett').click(function(){
-		$("h3").append();
-		$("h2").hide();
-		$(".darthsidious").appendTo("p");
-		$(".lukeskywalker").appendTo("p");
-		$(".darthvader").appendTo("p");
-		
+	//picking boba battle
+	$('#boba').click(function(){
+		$('h3').append('#yourCharacter')
+		$('h2').hide();
+		$('#sidious, #luke, #vader').appendTo('#yourEnemies');
 
 
 		// alert('it works!');
 
 	});
-	//picking emperor poopatine fight
-	$('.darthsidious').click(function(){
-		$("h3").append();
-		$("h2").hide();
-		$(".bobafett").appendTo("p");
-		$(".lukeskywalker").appendTo("p");
-		$(".darthvader").appendTo("p");
-
+	//picking emperor poopatine battle
+	$('#sidious').click(function(){
+		$('h3').append('#yourCharacter');  // thing to attach to.append(thing to attach) 
+		$('h2').hide();
+		$('#boba, #luke, #vader').appendTo('#yourEnemies');
 
 
 		// alert('yes!');
 
 	});
-	//picking uke fight
-	$('.lukeskywalker').click(function(){
-		$("h3").append();
-		$("h2").hide();
-		$(".bobafett").appendTo("p");
-		$(".darthsidious").appendTo("p");
-		$(".darthvader").appendTo("p");
-
+	//picking luke battle
+	$('#luke').click(function(){
+		$('h3').append('#yourCharacter');
+		$('h2').hide();
+		$('#boba, #sidious, #vader').appendTo('#yourEnemies');
 
 
 		// alert('thank you!');
 
 	});
-	//picking vader fight
-	$('.darthvader').click(function(){
-		$("h3").append();
-		$("h2").hide();
-		$(".bobafett").appendTo("p");
-		$(".darthsidious").appendTo("p");
-		$(".lukeskywalker").appendTo("p");
-
+	//picking vader battle
+	$('#vader').click(function(){
+		$('h3').append('#yourCharacter');
+		$('h2').hide();
+		$('#boba, #sidious, #luke').appendTo('#yourEnemies');
 
 
 		// alert('alright!');
 
 	});
 
-	$('.btn-group').click(function() {
-			$('.players').restart();
-		});
+		//attack button
+	$('.attk-btn-default').click(function() {
+		this.yourCharacter = this.players +6;
+	});
+
+
+		//restart game
+	$('.restart-btn-default').click(function(){
+  		document.location.reload(true);
+	});
+
 
 	
 });
